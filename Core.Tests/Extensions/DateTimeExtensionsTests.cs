1f455c89b81ed2ee09d50fb16e59a6626448469b
Gian Luca da Silva Figueiredo <luca.gian16@outlook.com>
2022-07-29 23:40:55 -0300

    [03] Incluir testes da extensão de dateTime

---
+++ Core.Tests/Extensions/DateTimeExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Core.Tests.Extensions
+{
+    using Core.Extensions;
+
+    using System;
+
+    using Xunit;
+
+    public class DateTimeExtensionsTests
+    {
+        [Fact]
+        public void ShouldReturnUnixDateFormat()
+        {
+            var expected = 1659139200;
+            var date = new DateTime(2022, 07, 30, 00, 00, 00);
+            var result = date.ToUnixEpochDate();
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ShouldReturnUnixDateFormatInString()
+        {
+            var expected = "1659139200";
+            var date = new DateTime(2022, 07, 30, 00, 00, 00);
+            var result = date.ToUnixEpochDateToString();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
