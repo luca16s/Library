fd518e84b557a947efe163910d341a6069105f22
Gian Luca da Silva Figueiredo <luca.gian16@outlook.com>
2022-11-07 10:55:02 -0300

    Remover nullable das interfaces (#323)

--- Core/Interfaces/Services/IService.cs
+++ Core/Interfaces/Services/IService.cs
@@ -51,7 +51,7 @@ namespace Core.Interfaces.Services
         /// <returns>
         /// Todas as entidades.
         /// </returns>
-        IQueryable<TEntity?> GetAll(int amount);
+        IQueryable<TEntity> GetAll(int amount);
 
         /// <summary>
         /// Retorna uma entidade com base em um identificador de forma assíncrona.
@@ -62,7 +62,7 @@ namespace Core.Interfaces.Services
         /// <returns>
         /// Entidade encontrada.
         /// </returns>
-        Task<TEntity?> Get(TId id);
+        Task<TEntity> Get(TId id);
 
         /// <summary>
         /// Atualiza uma entidade com base em um identificador passado de forma assíncrona.
@@ -84,6 +84,6 @@ namespace Core.Interfaces.Services
         /// <returns>
         /// Lista de itens encontrados.
         /// </returns>
-        IQueryable<TEntity?> Search(Expression<Func<TEntity, bool>> predicate);
+        IQueryable<TEntity> Search(Expression<Func<TEntity, bool>> predicate);
     }
 }
