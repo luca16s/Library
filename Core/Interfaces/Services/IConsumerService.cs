54cc6520373e533f00bfec5e733d1536142b038b
Gian Luca <luca.gian16@outlook.com>
2022-12-24 19:07:19 -0300

    [349] Incluir projeto de consumo de APIs (#357)
    * [349] Incluir projeto de consumo de APIs
    
    * [349] Corrigir nome do serviço e da classe de consumo

---
+++ Core/Interfaces/Services/IConsumerService.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Interfaces.Services
+{
+    public interface IConsumerService<TResponse>
+        where TResponse : notnull
+    {
+        Task<TResponse> GetItemAsync();
+
+        Task<IEnumerable<TResponse>> GetItemsAsync();
+    }
+}
