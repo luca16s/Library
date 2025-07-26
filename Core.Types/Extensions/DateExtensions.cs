118c7e4a0d9b592fe68a452b963d02891a42b29a
Gian Luca da Silva Figueiredo <luca.gian16@gmail.com>
2025-07-26 13:07:53 -0300

    - incluir extensão de data

---
+++ Core.Types/Extensions/DateExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Types.Extensions;
+public static class DateExtensions
+{
+    public static DateOnly ToDateOnly(
+        this DateTime dateTime
+    ) => new(dateTime.Year, dateTime.Month, dateTime.Day);
+}
