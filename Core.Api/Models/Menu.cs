8ed0a9ea02d44d8bb591fe537d2408b18e05efe5
Gian Luca da Silva Figueiredo <luca.gian16@gmail.com>
2025-08-14 07:00:43 -0300

    - Incluir classe de Menu do sistema

---
+++ Core.Api/Models/Menu.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Api.Models;
+public class Menu
+{
+    public int Index { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public bool IsDisable { get; set; } = false;
+    public string Icon { get; set; } = string.Empty;
+    public bool HasSubMenu { get; set; } = false;
+    public ICollection<Menu> SubMenus { get; set; } = [];
+    public string Route { get; set; } = string.Empty;
+    public string Breadcrumb { get; set; } = string.Empty;
+}
